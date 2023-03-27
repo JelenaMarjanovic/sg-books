3fa19c52994cd50d83c8b0baacaa23046706eae0
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-03-27 13:18:30 +0200

    Fetching a list of records - including introducing useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import BookCreate from './components/BookCreate';
@@ -6,6 +6,16 @@ import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
 
   const createBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', {
